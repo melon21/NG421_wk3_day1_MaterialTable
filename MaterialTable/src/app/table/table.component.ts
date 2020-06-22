c6e54215fc7fa2d85667a73b93840fd109cc3d69
Melinda Marino <melindamarino22@gmail.com>
2020-06-22 04:17:59 -0500

    save

--- MaterialTable/src/app/table/table.component.ts
+++ MaterialTable/src/app/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { People } from '../interfaces/people';
+import { PeopleService } from '../services/people-service.service';
 
 @Component({
   selector: 'app-table',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['Id', 'Name', 'Job', 'Email'];
+  dataSource: MatTableDataSource<People>;
+  constructor(private peopleSerice: PeopleService) { }
 
   ngOnInit(): void {
+  this.dataSource = new MatTableDataSource(this.peopleSerice.getMembers());
   }
 
 }
